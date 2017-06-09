f04e5359c39d8db033ffd98f7523dc366db1e5c9
user1 <usilitel00@gmail.com>
2017-06-09 17:01:49 +0300

    Initial commit

---
+++ src/VariablesExample.java
@@ -0,0 +1,19 @@
+public class VariablesExample {
+
+    boolean statusOn;
+    double javaVar = 2.34;
+
+    public static void main(String[] args) {
+        int itemsSold = 04;
+        double salary = 1.234e3;
+        float itemCost = 11.0f;
+        int i = 0xFd45, k$;
+        double _interestRate;
+    }
+
+    public void javaMethod() {
+        long simpleVar = 1_000_000_000_000L;
+        byte byteVar2 = 123;
+    }
+
+}
